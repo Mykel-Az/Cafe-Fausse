449e74998cc3ac1f63fe2f3d704e6c38f19604d7
Michael Azeez-Adekanmbi <137731808+Mykel-Az@users.noreply.github.com>
2026-03-02 02:35:15 +0100

    Update api.js

--- cafe-fausse-frontend/src/services/api.js
+++ cafe-fausse-frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const BASE_URL = "https://cafe-fausse-p7er.onrender.com"
-// "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL
 
 export async function apiFetch(endpoint, options = {}) {
     const response = await fetch(`${BASE_URL}${endpoint}`, {
